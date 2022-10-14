4a65bd6fed1edc4dfd78ffa70691f7ba65d2dfc3
Njwin <njwinhassanibrahim@gmail.com>
2022-10-13 23:47:59 -0700

    Task 6 new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,9 +14,9 @@ int main(void)
 	float afloat;
 
 	printf("Size of a char: %1u byte(s)\n", sizeof(acharacter));
-        printf("Size of a int: %1u byte(s)\n", sizeof(ainteger));
+        printf("Size of an int: %1u byte(s)\n", sizeof(ainteger));
         printf("Size of a long int: %1u byte(s)\n", sizeof(along));
-        printf("Size of a long long: %1u byte(s)\n", sizeof(alonglong));
+        printf("Size of a long long int: %1u byte(s)\n", sizeof(alonglong));
         printf("Size of a float: %1u byte(s)\n", sizeof(afloat));
        
         return (0);
