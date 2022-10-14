9b2b78e88ea0ea028cd210e376c9b90c086ac135
Njwin <njwinhassanibrahim@gmail.com>
2022-10-14 09:41:38 -0700

    Task 8

---
+++ 101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	writ(2,"and that plece of is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
